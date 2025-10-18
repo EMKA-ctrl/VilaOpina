1d725891f629041c2595e0147d26ec5e9e4aff62
Marc <sebastiamarc@gmail.com>
2025-10-18 18:14:20 +0200

    dummy

--- src/halfDonut.tsx
+++ src/halfDonut.tsx
@@ -7,15 +7,16 @@ interface SemiDonutProps{
 function SemiDonut({ siPercentage }: SemiDonutProps){
 
     return(
-        <PieChart 
+        <PieChart
+            
             width={300}
             height={300}
             series={[
                 {
                 data: [{ id: 0, value: siPercentage, color: 'green' },{ id: 1, value: 100-siPercentage, color: 'red' }],
                 arcLabel:'value',
-                innerRadius: 30,
-                outerRadius: 60,
+                innerRadius: 35,
+                outerRadius: 80,
                 paddingAngle: 3,
                 cornerRadius: 5,
                 startAngle: -90,
@@ -23,7 +24,11 @@ function SemiDonut({ siPercentage }: SemiDonutProps){
                 cx: 150,
                 cy: 150,
                 }
+                
             ]}
+            sx={{width: '100%',
+          height: '100%',
+          '& svg': { width: '100%', height: '100%' },}}
         />
     )
 
